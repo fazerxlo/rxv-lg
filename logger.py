9ea5c57ecb93127ca10937eb56df23ee20f01d2f
fazerxlo <admin@susek.info>
2020-01-12 20:58:28 +0100

    Default logging to file, debug to console

--- logger.py
+++ logger.py
@@ -1,15 +1,28 @@
 import os
+import sys
 
 
 class RxvLogger:
 
+    LOG_FILE = sys.path[0] + '/rxvlg.log'
     DEBUG_ENABLED = (os.environ.get('RXV_DEBUG') is not None)
+
+
 
     @staticmethod
     def log(txt):
-        print(RxvLogger.__name__ + ": " + txt)
+        txt = RxvLogger.__name__ + ": " + txt
+        if RxvLogger.DEBUG_ENABLED:
+            print(txt)
+        else:
+            RxvLogger.write_to_file(txt)
 
     @staticmethod
     def debug(txt):
         if RxvLogger.DEBUG_ENABLED:
             RxvLogger.log("DEBUG: " + txt)
+
+    @staticmethod
+    def write_to_file(txt):
+        with open(RxvLogger.LOG_FILE, 'a+') as file:
+            file.write(txt+"\n")
